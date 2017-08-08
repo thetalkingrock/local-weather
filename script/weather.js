f044832f8d34cca9d8f43cdc4ef10da6feb6e6a2
John Rock <jtrock05@gmail.com>
2017-08-08 12:40:54 -0400

    Local tempature is now displayed

--- script/weather.js
+++ script/weather.js
@@ -9,15 +9,16 @@ $(document).ready(function(){
 	function gotLocation(position){
 		var longitude = position.coords.longitude;
 		var latitude = position.coords.latitude;
-		var codeCampJSON = $.getJSON("https://fcc-weather-api.glitch.me/api/current?lon=:" + longitude + "&lat=:" + latitude, function () {
-			
+		var codeCampJSON = $.getJSON("https://fcc-weather-api.glitch.me/api/current?lat=" + latitude
+		 + "&lon=" + longitude, function (data) {
+			$("#city-and-temp").prepend("<h4>" + Math.round(data["main"]["temp"]) + "&deg;" + " C" + "</h4>");
 		});
 		var googleJSON = $.getJSON("https://maps.googleapis.com/maps/api/geocode/json?latlng=" + latitude + "," + longitude + "&key=" + "AIzaSyAdK12U8IEM2GsHQpQUmadq4Ws5a_Ah0ko", function (data) {
 				
 				var cityName = data["results"][0]["address_components"][2]["long_name"];
 				var stateName = data["results"][0]["address_components"][4]["short_name"];
 				$("#main-container").css("visibility", "visible");
-				$("#city-name").append("<h3>" + cityName + ", " + stateName + "</h3>");
+				$("#city-and-temp").prepend("<h3>" + cityName + ", " + stateName + "</h3>");
 		});
 		
 		
