dd1146ff13cbd18f9c4c5621cb25261db9230bbe
John Rock <jtrock05@gmail.com>
2017-08-08 11:58:22 -0400

    Now able to display user's location

--- script/weather.js
+++ script/weather.js
@@ -7,13 +7,44 @@ $(document).ready(function(){
 	}
 	
 	function gotLocation(position){
-		$("h1").css("visibility", "visible");
-		$("h1").html("Location accessed");
+		var longitude = position.coords.longitude;
+		var latitude = position.coords.latitude;
+		var codeCampJSON = $.getJSON("https://fcc-weather-api.glitch.me/api/current?lon=:" + longitude + "&lat=:" + latitude, function () {
+			
+		});
+		var googleJSON = $.getJSON("https://maps.googleapis.com/maps/api/geocode/json?latlng=" + latitude + "," + longitude + "&key=" + "AIzaSyAdK12U8IEM2GsHQpQUmadq4Ws5a_Ah0ko", function (data) {
+				
+				var cityName = data["results"][0]["address_components"][2]["long_name"];
+				var stateName = data["results"][0]["address_components"][4]["short_name"];
+				$("#main-container").css("visibility", "visible");
+				$("#city-name").append("<h3>" + cityName + ", " + stateName + "</h3>");
+		});
+		
+		
+		//var addressComponents = googleJSON["results"];
+		//console.log(typeof addressComponents);
+		/*	
+		var cityFound = false;
+		var cityName = "";
+		for(var x = 0; x < addressComponents.length; x++){
+			if(addressComponents[x]["types"] === "locality"){
+				cityFound = true;
+				cityName = addressComponents[x]["short_name"];
+			}
+		}
+		if(cityFound){
+			$("#city-name").text(cityName);
+		}else{
+			$("h1").text("Unable to find city");
+		}
+		$("#main-container").css("visibility", "visible");
+		*/
 	}
-	
 	function displayErrorMessage(){
-		$("h1").css("visibility", "visible");
-		$("h1").html("Location could not be accessed");	
+		$("#main-container").css("visibility", "visible");
+		$("h1").text("Location could not be accessed");	
 	}
 	
 });
+
+//google map api key= AIzaSyAdK12U8IEM2GsHQpQUmadq4Ws5a_Ah0ko
